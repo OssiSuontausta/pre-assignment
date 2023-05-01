575e82a822dc4f56a4634b226711507171388ca5
Ossi Suontausta <ossuon@hotmail.com>
2023-05-01 13:02:00 +0300

    Connected to database, tested connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Connected to database!");
+});
 
 app.listen(3001, () => {
     console.log("Server running on port 3001!");
